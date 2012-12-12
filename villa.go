655b9d368cd10684c13c2341321b492b5af1c317
David <david@rd.netease.com>
2012-12-12 12:23:17 +0800

    ADD more introduction for package introduction documents

--- villa.go
+++ villa.go
@@ -1,2 +1,4 @@
-// Package villa contains some helper data structures.
+// Package villa contains some helper data structures: array list, priority queue.
+// By using array-list related structs, some mutable list can be easily maintained.
+// The priority queue related structs provides a ready to use interface instead of the standard interface.
 package villa
