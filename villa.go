4b4887119c74d307a696a6bf1ae34e328fb471fe
David <david@rd.netease.com>
2012-12-13 15:59:19 +0800

    MOD comments of package

--- villa.go
+++ villa.go
@@ -1,6 +1,6 @@
-// Package villa contains some helper data structures: priority queue(interface{} and int as elements), array list(with sort interface and binary search supporting).
+// Package villa contains some helper data structures: priority queue(interface{} and int as elements), slice wrapper(with sort interface and binary search supporting).
 //
-// By using array-list related structs, some mutable list can be easily maintained. Sort and binary-search algorithm can be easily imposed(with a custemizable comparator function).
+// By using Slice, IntSlice, FloatSlice or ComplexSlice types, some mutable list can be easily maintained. Sort and binary-search algorithm can be easily imposed(with a custemizable comparator function).
 //
 // The priority queue related structs provides a ready to use interface instead of the standard interface.
 package villa
