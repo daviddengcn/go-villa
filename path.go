41bbedb2560f368a4cd5b018d34d8fd7b94fb2ff
David <daviddengcn@163.com>
2013-02-03 13:50:16 +0800

    ADD some methods for Path

--- path.go
+++ path.go
@@ -1,10 +1,22 @@
 package villa
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+)
 
 // Path is a wrapper for a path in the OS
 type Path string
 
 func (p Path) Join(elem ...string) Path {
-    return Path(filepath.Join(append([]string{string(p)}, elem...)...))
+	return Path(filepath.Join(append([]string{string(p)}, elem...)...))
 }
+
+func (p Path) Mkdir(perm os.FileMode) error {
+	return os.Mkdir(string(p), perm)
+}
+
+func (p Path) MkdirAll(perm os.FileMode) error {
+	return os.MkdirAll(string(p), perm)
+}
+
