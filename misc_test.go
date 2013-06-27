55617450c1c0de0f11d70cb81d4a2e408c18e0ea
David <david@rd.netease.com>
2013-06-27 20:15:18 +0800

    ADD missing file

---
+++ misc_test.go
@@ -0,0 +1,15 @@
+package villa
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestNestedError(t *testing.T) {
+	defer __(o_())
+
+	orgErr := errors.New("Original Error")
+	ne := NestErrorf(NestErrorf(orgErr, "Level %d", 1), "Nested again")
+	AssertEquals(t, "ne.Error", ne.Error(), "Nested again: Level 1: Original Error")
+	AssertEquals(t, "ne.Deepest", ne.Deepest(), orgErr)
+}
